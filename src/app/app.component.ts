5a234a2e3aa295b0c9efa23f2ff9814ecafd8e2e
shackgerc <sagerprogramer@gmail.com>
2024-04-02 18:20:19 -0300

    Add participant registration Second step.
    Add profile photo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {PersonalInfoComponent} from "./components/participants/registration/personal-info/personal-info.component";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'rr-root',
   standalone: true,
-  imports: [RouterOutlet, PersonalInfoComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/participants/registration/profile-photo/profile-photo.component.ts
@@ -0,0 +1,67 @@
+import {Component, inject} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {DomSanitizer} from "@angular/platform-browser";
+import {NgClass} from "@angular/common";
+import {AuthService} from "../../../../services/auth.service";
+import {Router} from "@angular/router";
+import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
+import {
+  CropperModalResult,
+  ImageCropperModalComponent
+} from "../../../shared/image-cropper/image-cropper-modal.component";
+
+@Component({
+  selector: 'rr-profile-photo',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgClass
+  ],
+  templateUrl: './profile-photo.component.html',
+  styleUrl: './profile-photo.component.css'
+})
+export class ProfilePhotoComponent {
+  uploadedPhotoPreview: any;
+  private uploadedPhoto: File | null = null;
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+  private modalService: NgbModal = inject(NgbModal);
+
+  private sanitizer: DomSanitizer = inject(DomSanitizer);
+
+  capturePhoto(event: any) {
+     let modalRef: NgbModalRef = this.modalService.open(ImageCropperModalComponent,
+       {centered: true, size: "xl"});
+     modalRef.componentInstance.imageFile = event.target.files[0];
+     modalRef.componentInstance.croppedResult.subscribe(
+       (value: CropperModalResult) => {
+         let sanitizedImageUrl = this.sanitizer.bypassSecurityTrustUrl(value.imageUrl);
+         this.uploadedPhotoPreview = sanitizedImageUrl;
+         this.imageUrlToFile(value.imageUrl, "profilePhoto").then(
+           file => this.uploadedPhoto = file
+         ).catch(
+           // TODO: ADD A TOAST TO INFORM ABOUT THE ERROR
+           error => console.error(error)
+         );
+       }
+     );
+  }
+
+  imageUrlToFile = async function (imageUrl: string, fileName: string): Promise<File> {
+    // Fetch image data from URL
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+
+    // Create File object
+    return new File([blob], fileName, {type: blob.type});
+  }
+
+  onClickNext() {
+    if (this.uploadedPhoto) {
+      this.authService.setParticipantRegistrationRequestPhoto(this.uploadedPhoto);
+    }
+
+    // TODO: DEFINE ROTING
+    this.router.navigate([""]);
+  }
+}
