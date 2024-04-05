077d820f35fad8f381cd901d9e5d814d5d19dee6
shackgerc <sagerprogramer@gmail.com>
2024-04-04 21:14:09 -0300

    Add basic participant registration routing config.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {PersonalInfoComponent} from "./components/participants/registration/personal-info/personal-info.component";
+import {ProfilePhotoComponent} from "./components/participants/registration/profile-photo/profile-photo.component";
+import {
+  FavoriteActivitiesComponent
+} from "./components/participants/registration/favorite-activities/favorite-activities.component";
+import {
+  EmailConfirmationComponent
+} from "./components/participants/registration/email-confirmation/email-confirmation.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'participant',
+    children: [
+      { path: 'account', children: [
+          {path: 'registration', component: PersonalInfoComponent},
+          {path: 'photo', component: ProfilePhotoComponent},
+          {path: 'activities', component: FavoriteActivitiesComponent},
+          {path: 'confirmEmail/:userId', component: EmailConfirmationComponent}
+        ]
+      }
+    ]
+  }
+];
