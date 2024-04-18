6a904226a49e75025e9682ebf835389e4ae500a3
shackgerc <sagerprogramer@gmail.com>
2024-04-18 18:15:34 -0300

    Add CanActivate Guard to protect participant-only routes.

---
+++ src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {AuthService} from "../../services/auth/auth.service";
+import {inject} from "@angular/core";
+import {catchError, map, of, tap} from "rxjs";
+
+export const AuthGuard: CanActivateFn = (route, state) => {
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  if(!authService.currentUserLoginOn.value) {
+    return router.parseUrl('/login');
+  }
+
+  // Check with the server if the token is valid.
+  return  authService.validateUserToken().pipe(
+    map((value) => {
+      if(value) {
+        return true;
+      }
+      return router.parseUrl('/login')
+    }),
+    catchError(err => of(router.parseUrl('/login')))
+  );
+};
