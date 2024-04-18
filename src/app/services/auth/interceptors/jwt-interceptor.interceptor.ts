9afcf1b423395b763e76bb4c8eaea1487f831b0b
shackgerc <sagerprogramer@gmail.com>
2024-04-18 18:12:59 -0300

    Change the jwt interceptor to obtain the jwt token from the currentUserLoginOnToken behavior subject instead of the session storage.

--- src/app/services/auth/interceptors/jwt-interceptor.interceptor.ts
+++ src/app/services/auth/interceptors/jwt-interceptor.interceptor.ts
@@ -1,7 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import {AuthService} from "../auth.service";
+import {inject} from "@angular/core";
 
 export const jwtInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
-  let token: string | null = localStorage.getItem('token');
+  const authService: AuthService = inject(AuthService);
+
+  let token: string | null = authService.currentUserLoginOnToken.value;
 
   if(token) {
     req = req.clone({headers: req.headers.append('Authorization', 'Bearer ' + token)})
