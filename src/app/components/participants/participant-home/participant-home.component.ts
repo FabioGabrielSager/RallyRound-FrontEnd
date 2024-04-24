14b2a114989be15a11e9033a22561835a5923465
shackgerc <sagerprogramer@gmail.com>
2024-04-24 16:10:01 -0300

    Add a modal call to confirm the login out.

--- src/app/components/participants/participant-home/participant-home.component.ts
+++ src/app/components/participants/participant-home/participant-home.component.ts
@@ -2,6 +2,8 @@ import {Component, inject, OnInit} from '@angular/core';
 import {NavbarComponent, NavBarItem} from "../../shared/navbar/navbar.component";
 import {AuthService} from "../../../services/auth/auth.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {AlertComponent} from "../../shared/alert/alert.component";
 
 @Component({
   selector: 'rr-participant-home',
@@ -17,6 +19,15 @@ export class ParticipantHomeComponent implements OnInit {
   navbarItems: NavBarItem[] = [];
   private route: ActivatedRoute = inject(ActivatedRoute);
   private authService: AuthService = inject(AuthService);
+  private mpAuthService: MPAuthService = inject(MPAuthService);
+  private modalService: NgbModal = inject(NgbModal);
+  private router: Router = inject(Router);
+  private toastService: ToastService = inject(ToastService);
+  private subs: Subscription = new Subscription();
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -38,9 +49,18 @@ export class ParticipantHomeComponent implements OnInit {
         href: "",
         bootstrapIconClasses: "bi bi-box-arrow-right",
         iconInLeftOfTitle: false,
-        onClick: () => this.authService.logout(),
+        onClick: () => {
+          const modal: NgbModalRef = this.modalService.open(AlertComponent, {centered: true, size: "sm"});
+          modal.componentInstance.isAConfirm = true;
+          modal.componentInstance.title = "Salir";
+          modal.componentInstance.bodyString = {textParagraphs: ["Seguro que quieres salir de RallyRound?"]};
+          modal.componentInstance.confirmBehavior = () => {
+            this.authService.logout();
+            this.router.navigate(["/home"])
+          }
+        },
       }
-    ]
+    ];
   }
 
   protected readonly matchMedia = matchMedia;
