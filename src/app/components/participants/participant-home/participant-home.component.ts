93f948ae5cbf9609bfb0fabe82a5998b1c5e6964
shackgerc <sagerprogramer@gmail.com>
2024-04-24 16:18:44 -0300

    Add logic to redirect the user to the createEvent view.

--- src/app/components/participants/participant-home/participant-home.component.ts
+++ src/app/components/participants/participant-home/participant-home.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {NavbarComponent, NavBarItem} from "../../shared/navbar/navbar.component";
 import {AuthService} from "../../../services/auth/auth.service";
 import {ActivatedRoute, Params, Router} from "@angular/router";
@@ -17,7 +17,7 @@ import {ToastService} from "../../../services/toast.service";
   templateUrl: './participant-home.component.html',
   styleUrl: './participant-home.component.css'
 })
-export class ParticipantHomeComponent implements OnInit {
+export class ParticipantHomeComponent implements OnInit, OnDestroy {
   username: string = "";
   navbarItems: NavBarItem[] = [];
   private route: ActivatedRoute = inject(ActivatedRoute);
@@ -66,5 +66,48 @@ export class ParticipantHomeComponent implements OnInit {
     ];
   }
 
-  protected readonly matchMedia = matchMedia;
+  onCreateEvent() {
+    this.subs.add(
+      this.mpAuthService.isAccountLinked().subscribe({
+        next: (result) => {
+          if (!result) {
+            const modal: NgbModalRef = this.modalService.open(AlertComponent, {centered: true, size: 'lg'});
+            modal.componentInstance.isAConfirm = true;
+            modal.componentInstance.title = "Vincular mercado pago";
+            modal.componentInstance.bodyString = {
+              textParagraphs: [
+                "Para crear un evento primero debes vincular tu cuenta de mercado pago.",
+                "¿Quieres vincular tu cuenta de mercado pago?"
+              ]
+            };
+            modal.componentInstance.confirmBehavior = () => {
+              this.subs.add(
+                this.mpAuthService.getAuthUrl().subscribe({
+                  next: (url: string) => {
+                    const width = 600;
+                    const height = 800;
+                    const left = window.innerWidth / 2 - width / 2;
+                    const top = window.innerHeight / 2 - height / 2;
+                    const features =
+                      `width=${width},height=${height},left=${left},top=${top},resizable=yes,scrollbars=yes,status=yes`;
+                    window.open(url, '_blank', features);
+                  },
+                  error: err => {
+                    this.toastService.show("Hubo un error al intentar recuperar la dirección de autenticación " +
+                      "de mercado pago, por favor inténtelo más tarde.", "bg-danger");
+                  }
+                })
+              );
+            }
+          } else {
+            this.router.navigate(['/events/', { outlets: { events: ['create']}}]);
+          }
+        },
+        error: err => {
+          this.toastService.show("Hubo un error al intentar verificar su vinculación con mercado pago, " +
+            "por favor inténtelo más tarde.", "bg-danger");
+        }
+      })
+    );
+  }
 }
