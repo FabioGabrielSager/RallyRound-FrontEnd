982cfe346b84c37650c92c230995ee8f2bcac4f1
shackgerc <sagerprogramer@gmail.com>
2024-05-09 20:38:42 -0300

    Update EventResumeCard component to show the event inscription status if it is not null.

--- src/app/components/events/event-resume-card/event-resume-card.component.ts
+++ src/app/components/events/event-resume-card/event-resume-card.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {DatePipe} from "@angular/common";
 import {HourPipe} from "../../../pipe/hour.pipe";
 import {EventResumeDto} from "../../../models/event/eventResumeDto";
+import {EventInscriptionStatus} from "../../../models/event/eventInscriptionStatus";
 
 @Component({
   selector: 'rr-event-resume-card',
@@ -14,10 +15,11 @@ import {EventResumeDto} from "../../../models/event/eventResumeDto";
   styleUrl: './event-resume-card.component.css'
 })
 export class EventResumeCardComponent {
-    @Input() eventResume: EventResumeDto = {} as EventResumeDto;
-    @Output() onSeeEvent: EventEmitter<string> = new EventEmitter();
+  @Input() eventResume!: EventResumeDto;
+  @Output() onSeeEvent: EventEmitter<string> = new EventEmitter();
 
-    protected readonly Array = Array;
+  protected readonly Array = Array;
+  protected readonly EventInscriptionStatus = EventInscriptionStatus;
 
   onViewEventClick(eventId: string) {
     this.onSeeEvent.emit(eventId);
