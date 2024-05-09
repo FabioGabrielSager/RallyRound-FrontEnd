1947098688e639cfd77c7a536e63b8bcfa438f38
shackgerc <sagerprogramer@gmail.com>
2024-05-09 20:24:39 -0300

    Update MPAuthService.
    Configure requested routes to the new API endpoints.

--- src/app/services/rallyroundapi/mercadopago/mpauth.service.ts
+++ src/app/services/rallyroundapi/mercadopago/mpauth.service.ts
@@ -7,15 +7,15 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class MPAuthService {
-  private baseUrl: string = environment.RR_API_BASE_URL + '/mp/auth';
+  private baseUrl: string = environment.RR_API_BASE_URL + '/mp';
   private httpClient: HttpClient = inject(HttpClient);
   constructor() { }
 
   isAccountLinked(): Observable<boolean> {
-    return this.httpClient.get<boolean>(this.baseUrl + "/account/linked/");
+    return this.httpClient.get<boolean>(this.baseUrl + "/auth/account/linked/");
   }
 
   getAuthUrl(): Observable<string> {
-    return this.httpClient.get<string>(this.baseUrl + "/url/", { responseType: 'text' as 'json' });
+    return this.httpClient.get<string>(this.baseUrl + "/auth/url/", { responseType: 'text' as 'json' });
   }
 }
