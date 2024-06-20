3eeb628a534dcc364a2584c989b365e8f912782b
shackgerc <sagerprogramer@gmail.com>
2024-06-20 14:53:12 -0300

    Add filters to the user events view.

--- src/app/components/events/my-events/my-events.component.ts
+++ src/app/components/events/my-events/my-events.component.ts
@@ -1,20 +1,23 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {EventResumeDto} from "../../../models/event/eventResumeDto";
 import {EventResumeCardComponent} from "../event-resume-card/event-resume-card.component";
 import {
-  NgbCollapse, NgbDropdown, NgbDropdownMenu, NgbDropdownToggle,
+  NgbCollapse,
+  NgbDropdown,
+  NgbDropdownMenu,
+  NgbDropdownToggle,
   NgbModal,
   NgbModalRef,
   NgbPagination,
   NgbPaginationNext,
-  NgbPaginationPages, NgbPaginationPrevious
+  NgbPaginationPages,
+  NgbPaginationPrevious
 } from "@ng-bootstrap/ng-bootstrap";
-import {Subscription} from "rxjs";
+import {catchError, debounceTime, filter, ObservableInput, of, Subscription, switchMap} from "rxjs";
 import {Router, RouterLink} from "@angular/router";
 import {AlertComponent} from "../../shared/alert/alert.component";
 import {MPAuthService} from "../../../services/rallyroundapi/mercadopago/mpauth.service";
 import {ToastService} from "../../../services/toast.service";
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {SearchResultsListComponent} from "../../shared/search-results-list/search-results-list.component";
 import {NgClass} from "@angular/common";
 import {EventService} from "../../../services/rallyroundapi/event.service";
@@ -22,6 +25,10 @@ import {EventsResumesPage} from "../../../models/event/EventsResumesPage";
 import {
   CreatedEventsInscriptionTrendStatsModalComponent
 } from "../created-events-inscription-trend-stats-modal/created-events-inscription-trend-stats-modal.component";
+import {MatchedActivities} from "../../../models/common/MatchedActivities";
+import {Place} from "../../../models/location/place";
+import {RrActivityService} from "../../../services/rallyroundapi/rr-activity.service";
+import {MapApiService} from "../../../services/location/map-api.service";
 
 @Component({
   selector: 'rr-my-events',
@@ -45,36 +52,137 @@ import {
   styleUrl: './my-events.component.css'
 })
 export class MyEventsComponent implements OnInit {
-  isMyCreatedEventsPageSelected: boolean = false;
-  eventsAreLoading: boolean = false;
-  events!: EventsResumesPage;
-  actualPage: number = 0;
   private eventService: EventService = inject(EventService);
   private subs: Subscription = new Subscription();
   private mpAuthService: MPAuthService = inject(MPAuthService);
   private modalService: NgbModal = inject(NgbModal);
   private toastService: ToastService = inject(ToastService);
+  private activityService: RrActivityService = inject(RrActivityService);
+  private mapApiService: MapApiService = inject(MapApiService);
   private router: Router = inject(Router);
+  isMyCreatedEventsPageSelected: boolean = false;
+  eventsAreLoading: boolean = false;
+  events!: EventsResumesPage;
+  actualPage: number = 0;
   filtersForm!: FormGroup;
   private fb: FormBuilder = inject(FormBuilder);
 
+  suggestedActivities: string[] = [];
+  anActivityWasSelected: boolean = false;
+  selectedActivity: string = "";
+
+  formattedNames: string[] = [];
+  formattedNamePlaces: Map<string, Place> = new Map<string, Place>([]);
+  selectedPlace: Place | undefined = undefined;
+  private aLocationWasSelected: boolean = false;
+
+  isFiltersFormCollapsed: boolean = true;
+
   ngOnInit(): void {
     this.filtersForm = this.fb.group({
-      activity: ['Actividad', Validators.required],
-      description: ['', [Validators.required, Validators.maxLength( 2000)]],
-      address: ['', Validators.required],
-      streetNumberCheckBox: [''],
-      streetNumber: [''],
-      date: ['', Validators.required],
-      hours: this.fb.array([]),
-      duration: ['', Validators.required],
-      durationUnit: ['HOUR', Validators.required],
-      participants: ['1', [Validators.required, Validators.min(1)]],
-      inscriptionPrice: ['', [Validators.required, Validators.min(1)]],
-      participantOrganizer: ['organizer', Validators.required],
-      selectedHour: []
+      activity: [],
+      location: [''],
+      dateFrom: [new Date().toISOString().split('T')[0]],
+      dateTo: [ new Date().toISOString().split('T')[0]],
     });
-    this.getMyEvents();
+
+    // Activities autosuggestion
+    this.subs.add(
+      this.filtersForm.controls['activity'].valueChanges.pipe(
+        debounceTime(300),
+        filter(query => query.length >= 3),
+        switchMap( (query): ObservableInput<any> => {
+          if(query == "" || this.anActivityWasSelected) {
+            this.anActivityWasSelected = false;
+            return of(null);
+          }
+          return this.activityService.getMatchedActivities(query).pipe(
+            catchError(error => {
+              console.error('Error fetching autosuggestions:', error);
+              return of(error);
+            })
+          )
+        })
+      ).subscribe(
+        {
+          next: (value: null | MatchedActivities) => {
+            this.suggestedActivities = value != null ? value.activities : [];
+          }
+        }
+      )
+    );
+
+    // Locations autosuggestion
+    this.subs.add(
+      this.filtersForm.controls['location'].valueChanges.pipe(
+        debounceTime(300),
+        filter(query => query.length >= 3),
+        switchMap( (query): ObservableInput<any> => {
+          if(query == "" || this.aLocationWasSelected) {
+            this.aLocationWasSelected = false;
+            return of(null);
+          }
+          return this.mapApiService.getAutosuggestionPlaces(query).pipe(
+            catchError(error => {
+              console.error('Error fetching autosuggestions:', error);
+              return of(error);
+            })
+          )
+        })
+      ).subscribe({
+        next: (value: null | Place[]) => {
+          this.formattedNames = [];
+          this.formattedNamePlaces.clear();
+          if (value != null) {
+            this.selectedPlace = undefined;
+            value.forEach((p: Place) => {
+              let place: Place = new Place(p.__type, p.address, p.name);
+              let formattedName: string = place.getFormattedName();
+              this.formattedNames.push(formattedName);
+              this.formattedNamePlaces.set(formattedName, p);
+            });
+          }
+        },
+        error: err => {
+          this.toastService.show("Hubo un error al recuperar lugares, por favor intentelo más tarde.",
+            "bg-danger");
+          console.error(err);
+        }
+      })
+    );
+
+    this.getMyEvents(
+      this.selectedActivity, this.selectedPlace?.address.neighborhood,
+      this.selectedPlace?.address.locality, this.selectedPlace?.address.adminDistrict2,
+      this.selectedPlace?.address.adminDistrict, this.filtersForm.controls["dateFrom"].value,
+      this.filtersForm.controls["dateTo"].value
+    );
+  }
+
+  onBlurActivityInput() {
+    setTimeout(() => {
+      this.suggestedActivities = [];
+    }, 100)
+  }
+
+  onClickActivitySearchResult($event: any) {
+    this.anActivityWasSelected = true;
+    this.selectedActivity = $event.target.innerText;
+  }
+
+  onBlurLocationInput() {
+    setTimeout(() => {
+      this.formattedNames = [];
+      this.formattedNamePlaces.clear();
+    }, 200);
+  }
+
+  onClickPlaceSearchResult($event: any) {
+    this.selectedPlace = this.formattedNamePlaces.get($event.target.innerText);
+    this.filtersForm.controls['location'].setValue($event.target.innerText);
+    this.aLocationWasSelected = true;
+    this.formattedNames = [];
+    this.formattedNamePlaces.clear();
   }
 
   onSelectPage() {
@@ -82,22 +190,32 @@ export class MyEventsComponent implements OnInit {
     this.searchEvents();
   }
 
-  private searchEvents() {
+  searchEvents() {
     if(!this.isMyCreatedEventsPageSelected) {
-      this.getMyEvents();
+      this.getMyEvents(this.filtersForm.controls["activity"].value, this.selectedPlace?.address.neighborhood,
+        this.selectedPlace?.address.locality, this.selectedPlace?.address.adminDistrict2,
+        this.selectedPlace?.address.adminDistrict, this.filtersForm.controls["dateFrom"].value,
+        this.filtersForm.controls["dateTo"].value);
     } else {
-      this.getMyCreatedEvents();
+      this.getMyCreatedEvents(
+        this.filtersForm.controls["activity"].value, this.selectedPlace?.address.neighborhood,
+        this.selectedPlace?.address.locality, this.selectedPlace?.address.adminDistrict2,
+        this.selectedPlace?.address.adminDistrict, this.filtersForm.controls["dateFrom"].value,
+        this.filtersForm.controls["dateTo"].value
+      );
     }
 
     this.actualPage = this.events.page;
   }
 
-  private getMyEvents() {
+  private getMyEvents(activity: string | undefined, neighborhood: string | undefined,
+                      locality: string | undefined, adminSubdistrict: string | undefined, adminDistrict: string | undefined,
+                      dateFrom: string | null, dateTo: string | null) {
     // TODO: Add the required filters for this parameters
     this.eventsAreLoading = true;
     this.eventService.getCurrentUserParticipatingEvents(null, null, null,
-      undefined, undefined, undefined,
-      undefined, undefined, null, null, []).subscribe(
+      activity, neighborhood, locality,
+      adminSubdistrict, adminDistrict, dateFrom, dateTo, []).subscribe(
       {
         next: value => {
           this.events = value;
@@ -111,11 +229,13 @@ export class MyEventsComponent implements OnInit {
     );
   }
 
-  private getMyCreatedEvents() {
+  private getMyCreatedEvents(activity: string | undefined, neighborhood: string | undefined, locality: string | undefined,
+                             adminSubdistrict: string | undefined, adminDistrict: string | undefined, dateFrom: string | null,
+                             dateTo: string | null) {
     // TODO: Add the required filters for this parameters
     this.eventsAreLoading = true;
-    this.eventService.getCurrentUserCreatedEvents(undefined, undefined, undefined,
-      undefined, undefined, null, null, []).subscribe(
+    this.eventService.getCurrentUserCreatedEvents(activity, neighborhood, locality,
+      adminSubdistrict, adminDistrict, dateFrom, dateTo, []).subscribe(
       {
         next: value => {
           this.events = value;
